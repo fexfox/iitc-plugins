e7214936c43e562dc414e4847b6b769532efd159
udnp <udnp@sawdagger.nagoya>
2016-03-15 22:37:10 +0900

    [A] added @updateURL and @downloadURL for this plugin user script.

---
+++ built/comm-filter.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @id             iitc-plugin-comm-filter@udnp
+// @name           IITC plugin: COMM Filter
+// @author         udnp
+// @category       COMM
+// @version        0.3.3.20160315.115849
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @source         https://github.com/udnp/iitc-plugins
+// @updateURL      https://github.com/udnp/iitc-plugins/raw/comm-filter-plugin/develop/built/comm-filter.meta.js
+// @downloadURL    https://github.com/udnp/iitc-plugins/raw/comm-filter-plugin/develop/built/comm-filter.user.js
+// @description    [local-2016-03-15-115849] COMM Filter
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @include        https://www.ingress.com/mission/*
+// @include        http://www.ingress.com/mission/*
+// @match          https://www.ingress.com/mission/*
+// @match          http://www.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
