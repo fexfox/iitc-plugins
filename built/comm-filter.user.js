2955fa1ce92a03d552ef9c635d44d9cdc5f47abf
udnp <udnp@sawdagger.nagoya>
2016-03-29 00:17:28 +0900

    [C] changed plug-in's URL for release.

--- built/comm-filter.user.js
+++ built/comm-filter.user.js
@@ -6,8 +6,8 @@
 // @version        0.4.0.20160328.150652
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @source         https://github.com/udnp/iitc-plugins
-// @updateURL      https://github.com/udnp/iitc-plugins/raw/comm-filter-plugin/develop/built/comm-filter.meta.js
-// @downloadURL    https://github.com/udnp/iitc-plugins/raw/comm-filter-plugin/develop/built/comm-filter.user.js
+// @updateURL      https://github.com/udnp/iitc-plugins/raw/master/built/comm-filter.meta.js
+// @downloadURL    https://github.com/udnp/iitc-plugins/raw/master/built/comm-filter.user.js
 // @description    [udnp-2016-03-28-150652] COMM Filter
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
