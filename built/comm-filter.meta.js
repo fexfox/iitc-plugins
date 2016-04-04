1b8b808ccc5d29a7e9b81724771e095cea0cd99b
udnp <udnp@sawdagger.nagoya>
2016-04-04 11:55:07 +0900

    [C] updated version.

--- built/comm-filter.meta.js
+++ built/comm-filter.meta.js
@@ -3,12 +3,12 @@
 // @name           IITC plugin: COMM Filter
 // @author         udnp
 // @category       COMM
-// @version        0.5.0.20160404.20520
+// @version        0.5.1.20160404.25356
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @source         https://github.com/udnp/iitc-plugins
-// @updateURL      https://github.com/udnp/iitc-plugins/raw/master/built/comm-filter.meta.js
-// @downloadURL    https://github.com/udnp/iitc-plugins/raw/master/built/comm-filter.user.js
-// @description    [udnp-2016-04-04-020520] COMM Filter
+// @updateURL      https://github.com/udnp/iitc-plugins/raw/comm-filter-plugin/develop/built/comm-filter.meta.js
+// @downloadURL    https://github.com/udnp/iitc-plugins/raw/comm-filter-plugin/develop/built/comm-filter.user.js
+// @description    [udnp-2016-04-04-025356] COMM Filter
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
