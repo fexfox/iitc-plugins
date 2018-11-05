e5050f92f77dc6b2435f14d2e5f70fcfd7853d69
fexfox <fexfox@vip.qq.com>
2018-11-05 13:16:34 +0800

    fix new url for intel

--- built/comm-filter.user.js
+++ built/comm-filter.user.js
@@ -9,14 +9,14 @@
 // @updateURL      https://github.com/udnp/iitc-plugins/raw/master/built/comm-filter.meta.js
 // @downloadURL    https://github.com/udnp/iitc-plugins/raw/master/built/comm-filter.user.js
 // @description    [udnp-2017-05-08-134802] COMM Filter
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
-// @include        https://www.ingress.com/mission/*
-// @include        http://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
-// @match          http://www.ingress.com/mission/*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
 
